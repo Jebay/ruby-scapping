d395fe51c2569d3eababa3d4d182b6b6ef7fa3ad
Jean-Baptiste JOLY <jebay.joly@gmail.com>
2021-09-16 17:32:10 +0400

    FEAT: update filtering with css

--- curl-v2.rb
+++ curl-v2.rb
@@ -1,11 +1,12 @@
-require 'net/http'
-require 'open-uri'
-require 'nokogiri'
+#! /usr/bin/env ruby
 
+require 'nokogiri'
+require 'open-uri'
 
 uri = Nokogiri::HTML(URI.open('https://rnm.franceagrimer.fr/prix?M2503:MARCHE'))
 
 # https://readysteadycode.com/howto-parse-html-with-ruby-and-nokogiri
-uri.search('a').each do |link|
+
+uri.css('a').each do |link|
     puts link.content
   end
