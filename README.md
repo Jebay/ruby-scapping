920797291057180081f5aa4cbe574520b821bdbd
Jean-Baptiste JOLY <jebay.joly@gmail.com>
2021-09-21 15:34:07 +0400

    FEAT: update Readme and delete old code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ruby-scapping
-ruby-scapping
+
+## Objective :
+Make available the prices of fruit and vegetables in France through an application or a website.
+
+## Steps
+1. [x] Collecte Data
+2. [ ] Fill in the database
+3. [ ] UX/UI
+4. [ ] Hosting the app
+
+## Collaboration
+If you want to join my project, fill free to ask.
+
+Source : FranceAgriMer – Original data downloaded from the [FranceAgriMer](https://rnm.franceagrimer.fr/prix?M2503:MARCHE) page
--- curl.rb
+++ curl.rb
@@ -1,8 +1,21 @@
-require 'net/http'
-require 'uri'
+#! /usr/bin/env ruby
 
-uri = URI.parse("https://rnm.franceagrimer.fr/prix?M2503:MARCHE")
-response = Net::HTTP.get_response(uri)
+require 'nokogiri'
+require 'open-uri'
 
-# puts response.code
-puts response.body
+def web_scraper(url)
+    uri = Nokogiri::HTML(URI.open(url))
+
+    uri.css('td').each do |link|
+        filter = /[[:upper:]]{3,} [[A-Z]]{0,1}[a-z]+/.match(link.content) # Filter fruits & vegetables
+        filter_price = /[[\-+]]{0,1}[[:digit:]]{1,}[[.]]{0,1}[[:digit:]]{0,}/.match(link.content) # Filter price
+    
+    if filter.to_s.length > 0
+            puts filter
+    end
+    if filter_price.to_s.length > 0
+            puts filter_price
+    end
+end
+
+web_scraper('https://rnm.franceagrimer.fr/prix?M2503:MARCHE')
