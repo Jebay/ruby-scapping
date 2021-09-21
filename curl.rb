406ec72c18654782867ffe915f7bbfb83319dba3
Jean-Baptiste JOLY <jebay.joly@gmail.com>
2021-09-21 15:52:03 +0400

    FIX: fix file indentation

--- curl.rb
+++ curl.rb
@@ -9,12 +9,13 @@ def web_scraper(url)
     uri.css('td').each do |link|
         filter = /[[:upper:]]{3,} [[A-Z]]{0,1}[a-z]+/.match(link.content) # Filter fruits & vegetables
         filter_price = /[[\-+]]{0,1}[[:digit:]]{1,}[[.]]{0,1}[[:digit:]]{0,}/.match(link.content) # Filter price
-    
-    if filter.to_s.length > 0
+
+        if filter.to_s.length > 0
             puts filter
-    end
-    if filter_price.to_s.length > 0
+        end
+        if filter_price.to_s.length > 0
             puts filter_price
+        end
     end
 end
 
