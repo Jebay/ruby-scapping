5bc94ef3b39246cd181b0e17fc5feb62155e7c16
Jean-Baptiste JOLY <jebay.joly@gmail.com>
2021-09-21 15:13:32 +0400

    FEAT: add filter for Price

--- curl-v2.rb
+++ curl-v2.rb
@@ -10,12 +10,13 @@ def web_scraper(url)
 
     uri.css('td').each do |link|
         filter = /[[:upper:]]{3,} [[A-Z]]{0,1}[a-z]+/.match(link.content) # Filter fruits & vegetables
-        # filter = /\d*\R{0}/.match(link.content)
+        filter_price = /[[\-+]]{0,1}[[:digit:]]{1,}[[.]]{0,1}[[:digit:]]{0,}/.match(link.content) # Filter price
     
     if filter.to_s.length > 0
             puts filter
-        end
-        # puts link.content
+    end
+    if filter_price.to_s.length > 0
+            puts filter_price
     end
 end
 
