1940edc14932a90adea6ae1bb7cb6a64dafe920d
codefactor-io <support@codefactor.io>
2023-01-27 14:19:25 +0000

    [CodeFactor] Apply fixes to commit 7bb0ba9

--- chatGPT.rb
+++ chatGPT.rb
@@ -7,28 +7,26 @@ require 'nokogiri'
 require 'open-uri'
 require_relative 'fruit'
 
-def web_scraper()
+def web_scraper
   # URL to scrape
-  page = Nokogiri::HTML(URI.parse("https://rnm.franceagrimer.fr/prix?M2503:MARCHE").open)
+  page = Nokogiri::HTML(URI.parse('https://rnm.franceagrimer.fr/prix?M2503:MARCHE').open)
 
   # Array to store the Fruit objects
   fruits = []
 
-  page.css("table tr").each do |row|
-    name = row.css("td:nth-child(1)").text.strip
-    price = row.css("td:nth-child(2)").text.strip
-    varia = row.css("td:nth-child(3)").text.strip
-    min = row.css("td:nth-child(4)").text.strip
-    max = row.css("td:nth-child(5)").text.strip
-    
+  page.css('table tr').each do |row|
+    name = row.css('td:nth-child(1)').text.strip
+    price = row.css('td:nth-child(2)').text.strip
+    varia = row.css('td:nth-child(3)').text.strip
+    min = row.css('td:nth-child(4)').text.strip
+    max = row.css('td:nth-child(5)').text.strip
+
     # Check that the price contains only numbers
-    if /^[0-9]/.match?(price)
-      fruits << Fruit.new(name, price, varia, min, max)
-    end
+    fruits << Fruit.new(name, price, varia, min, max) if /^[0-9]/.match?(price)
   end
 
   # Print the fruits array
   fruits.each(&:display)
-end 
+end
 
-web_scraper()
+web_scraper
